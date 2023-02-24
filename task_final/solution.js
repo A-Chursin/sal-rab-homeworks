c03d0c0158fd481c40c82ec57602b69c7f22b890
Anton Chursin <tacgonl@gmail.com>
2023-02-25 03:08:43 +0700

    task 4 complete

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,24 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    let data = {client: '', order: {}, goods: []};
 
     let countOfGoods = goods.length;
+    let goodsContainer = {title: '', count: 0} //создаем объект-контейнер
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i < countOfGoods; i += 1) {
+        goodsContainer.title = goods[i].title;
+        goodsContainer.count = goods[i].count;
+        data.goods.push(goodsContainer);
+        goodsContainer = {title: '', count: 0}; //очищаем контейнер, поскольку очевидно перезапись работает не так, как я представлял. Должен быть более элегантный способ...
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв. ${address.flat}`;
+    data.order.sum = sum;
 
-    data.client = 'Иван';
+    data.client = `${name} ${phone}`;
 
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data});
 
     return jsonData;
 }
